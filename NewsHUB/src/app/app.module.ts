b09fe3201e2249c0f6ff60f81ed2a1f5e2fda8cc
Kamil Cała <camil.kala@gmail.com>
2018-04-30 22:17:41 +0200

    #033_search | Dodano do navbaru pasek wyszukiwania wraz z ikona

--- NewsHUB/src/app/app.module.ts
+++ NewsHUB/src/app/app.module.ts
@@ -4,17 +4,19 @@ import { NgModule } from '@angular/core';
 
 import { HeaderComponent } from './header/header';
 import { MenuComponent } from './menu/menu';
+import { SearchComponent } from './searchBar/searchBar';
 import { ContenerComponent } from './contener/contener';
 
 @NgModule({
   declarations: [HeaderComponent,
     MenuComponent,
+    SearchComponent,
     ContenerComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [ HeaderComponent, MenuComponent, ContenerComponent ]
+  bootstrap: [ HeaderComponent, MenuComponent, SearchComponent, ContenerComponent ]
 })
 export class AppModule { }
