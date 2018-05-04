9c9661ea2111003114fb45ede97259a4ba031e22
Kamil Smolen <kamilxsmolen@gmail.com>
2018-05-04 17:33:47 +0200

    animowanie slider'a co 6 sekund

--- NewsHUB/src/app/article/article.ts
+++ NewsHUB/src/app/article/article.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {SliderComponent} from "../slider/slider";
 
 @Component({
   selector: 'article',
--- NewsHUB/src/app/slider/slider.ts
+++ NewsHUB/src/app/slider/slider.ts
@@ -5,4 +5,34 @@ import { Component } from '@angular/core';
   templateUrl: './slider.html',
   styleUrls: ['./slider.css']
 })
-export class SliderComponent {}
+export class SliderComponent {
+
+  slideIndex:number;
+
+  constructor(){
+    this.slideIndex = 1;
+  }
+
+  showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("mySlides") as HTMLCollectionOf<HTMLElement>;
+    let dots = document.getElementsByClassName("dot");
+    for (let i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    this.slideIndex++;
+    if (this.slideIndex > slides.length) {
+      this.slideIndex = 1
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace("active", "");
+    }
+    slides[this.slideIndex - 1].style.display = "block";
+    dots[this.slideIndex - 1].className += " active";
+  }
+}
+
+window.addEventListener('load', function () {
+  let slider = new SliderComponent()
+  window.setInterval(()=>slider.showSlides(),6000);
+});
