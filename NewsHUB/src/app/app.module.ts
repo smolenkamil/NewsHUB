2925d4b68d5348e0bfbfbaea2cf3d15ebd6adeae
Kamil Cała <camil.kala@gmail.com>
2018-05-08 22:18:19 +0200

    Zmiany zwiazane z firebasem

--- NewsHUB/src/app/app.module.ts
+++ NewsHUB/src/app/app.module.ts
@@ -20,7 +20,7 @@ import {SliderArticleComponent} from "./sliderArticle/sliderArticle";
     HeaderComponent,
     MenuComponent,
     SearchComponent,
-    // ProfileMenuComponent,
+    ProfileMenuComponent,
     ContenerComponent,
     SliderComponent,
     ArticleComponent,
