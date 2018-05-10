9c35a49291694bb59a1ebc529fefcb49691b19b4
Kamil Cała <camil.kala@gmail.com>
2018-05-10 18:12:31 +0200

    Proba implementacji logowania z uzyciem firebase

--- NewsHUB/src/app/contener2/contener2.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'kontener2',
-  templateUrl: './contener2.html',
-  styleUrls: ['../contener/contener.css']
-})
-export class Contener2Component {}
