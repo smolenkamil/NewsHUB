38822d1bad67d5517b87e27d5ba420a5db8520b9
Kamil Smolen <kamilxsmolen@gmail.com>
2018-05-14 01:53:33 +0200

    przeladowywanie artykulow

--- NewsHUB/src/app/article/article.ts
+++ NewsHUB/src/app/article/article.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
 import {NewsProvider} from "../logic/news";
 import {Article} from "../logic/article";
 
@@ -7,17 +7,26 @@ import {Article} from "../logic/article";
   templateUrl: './article.html',
   styleUrls: ['./article.css']
 })
-export class ArticleComponent implements OnInit{
+export class ArticleComponent implements OnInit, OnChanges{
 
   newsx: NewsProvider;
   article: Article;
+  @Input('cat')catNumber:number;
+
+  ngOnChanges(chang: SimpleChanges){
+    this.article = this.newsx.getArticle(chang.catNumber.currentValue);
+    this.newsx.eraseArticles();
+  }
 
   constructor(){
+    if(this.catNumber===0)
+      this.catNumber=0;
     this.newsx = new NewsProvider()
   }
 
   ngOnInit(){
-    this.article = this.newsx.getArticle();
+    this.article = this.newsx.getArticle(this.catNumber);
+    this.newsx.eraseArticles();
   }
 
 }
