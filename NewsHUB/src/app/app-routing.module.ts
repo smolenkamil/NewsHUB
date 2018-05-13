dda7070e2bbb2787e55c8783f02fd8842df72575
Kamil Smolen <kamilxsmolen@gmail.com>
2018-05-14 01:50:13 +0200

    routing

--- NewsHUB/src/app/app-routing.module.ts
+++ NewsHUB/src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import {HomepageComponent} from "./homepage/homepage.component";
 
 const routes: Routes = [
   {path: '', redirectTo: 'homepage', pathMatch: 'full'},
+  {path: 'homepage/:cat', component: HomepageComponent},
   {path: 'homepage', component: HomepageComponent},
   {path: 'profile', component: ProfileContainerComponent}
 ];
--- NewsHUB/src/app/homepage/homepage.component.ts
+++ NewsHUB/src/app/homepage/homepage.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-homepage',
@@ -6,7 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./homepage.component.css']
 })
 export class HomepageComponent implements OnInit {
-  constructor() {}
+  constructor(private route: ActivatedRoute) {}
 
-  ngOnInit() {}
+  catId: number;
+
+  ngOnInit() {
+    this.route.params.subscribe((params) => {
+      this.catId = params['cat'];
+    }
+  }
 }
