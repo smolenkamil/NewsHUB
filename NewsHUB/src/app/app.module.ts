8f145a74a70199a986a8ebb1a0608bc4a923758e
Kamil Smolen <kamilxsmolen@gmail.com>
2018-05-17 23:54:35 +0200

    dla Kamila Nakomitczekała

--- NewsHUB/src/app/app.module.ts
+++ NewsHUB/src/app/app.module.ts
@@ -3,10 +3,10 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {AngularFireModule} from 'angularfire2';
+import {AngularFireDatabaseModule} from 'angularfire2/database-deprecated';
 import {AngularFirestoreModule} from 'angularfire2/firestore';
 import {AngularFireAuthModule} from 'angularfire2/auth';
 import {AuthService} from './auth.service';
-import {AngularFireDatabaseModule} from 'angularfire2/database';
 import {HttpClientModule} from '@angular/common/http';
 
 import {MenuComponent} from './menu/menu';
@@ -17,7 +17,7 @@ import {DatePipeComponent, WeatherComponent} from './weather/weather';
 import {SliderArticleComponent} from "./sliderArticle/sliderArticle";
 import {AppComponent} from './app.component';
 import {ProfileContainerComponent} from "./profileContainer/profileContainer.component";
-import {UserDataComponent, UsersDataComponent} from "./userData/userData";
+import {UserDataComponent} from "./userData/userData";
 import {CategoriesChoiceComponent} from "./categoriesChoice/categoriesChoice";
 import {WeatherChoiceComponent} from "./weatherChoice/weatherChoice";
 import {HomepageComponent} from "./homepage/homepage.component";
@@ -59,14 +59,14 @@ import { environment } from '../environments/environment';
     HomepageComponent,
     AboutProjectComponent,
     BtnAboutProjectComponent,
-    LoginContainerComponent,
-    UsersDataComponent
+    LoginContainerComponent
+    // UsersDataComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     AppRoutingModule,
-    AngularFireModule.initializeApp(environment.firebase, 'NewsHUB'),
+    AngularFireModule.initializeApp(environment.firebase),
     AngularFireAuthModule,
     AngularFirestoreModule,
     AngularFireDatabaseModule,
--- NewsHUB/src/app/userData/userData.ts
+++ NewsHUB/src/app/userData/userData.ts
@@ -1,30 +1,14 @@
 import { Component } from '@angular/core';
-import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database-deprecated';
+import { AngularFireDatabase} from 'angularfire2/database-deprecated';
+import { FirebaseListObservable } from 'angularfire2/database-deprecated';
+
 
 @Component({
   selector: 'userData',
   templateUrl: './userData.html',
   styleUrls: ['./userData.css']
 })
-export class UserDataComponent {}
-
-class UsersData {
-  constructor(public name, public surname) { }
-}
-
-@Component({
-  selector: 'usersData',
-  template: `
-    <ul>
-      <li *ngFor="let data of userDatas | async">
-        <pre>{{ data | json }}</pre>
-      </li>
-    </ul>
-    
-  `
-})
-
-export class UsersDataComponent {
+export class UserDataComponent {
   public userDatas: FirebaseListObservable<UsersData[]>;
 
   constructor(db: AngularFireDatabase) {
@@ -33,9 +17,36 @@ export class UsersDataComponent {
 
   // private bookCounter = 0;
 
-  public AddUserData(): void {
-    let newData = new UsersData(`Kamil`, 'Cała');
+  addUserData(): void {
+    let newData = {
+      name:"Kamil Masakra",
+      surname:"Cotymaszwełbiedrewno"
+    }
     this.userDatas.push(newData);
   }
 }
+
+class UsersData {
+  name:string;
+  surname:string;
+  constructor(name, surname) {
+    this.name=name;
+    this.surname=surname;
+  }
+}
+
+// @Component({
+//   selector: 'usersData',
+//   template: `
+//     <ul>
+//       <li *ngFor="let data of userDatas | async">
+//         <pre>{{ data | json }}</pre>
+//       </li>
+//     </ul>
+//
+//   `
+// })
+//
+// export class UsersDataComponent {
+// }
 
