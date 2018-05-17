9fbdd201e70d02ae2cb399c0a22a19ec42b84e82
Kamil Smolen <kamilxsmolen@gmail.com>
2018-05-17 22:16:59 +0200

    weather bug fix

--- NewsHUB/src/app/weather/weather.ts
+++ NewsHUB/src/app/weather/weather.ts
@@ -24,8 +24,8 @@ export class WeatherComponent implements OnInit {
 
     this._weather.dailyForecast()
       .subscribe(res => {
-        let hum = res.main.humidity;
-        let clou = res.clouds.all;
+        let hum = res.main.humidity || 0;
+        let clou = res.clouds.all || 0;
         let restHum = 100 - hum;
         let restClou = 100 - clou;
         this.chart = new Chart('humidity', {
