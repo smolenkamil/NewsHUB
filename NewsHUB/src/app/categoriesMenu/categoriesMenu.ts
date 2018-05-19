38eef92814f211a60ac2b292ba1b12a59c0f5d37
Kamil Smolen <kamilxsmolen@gmail.com>
2018-05-19 15:20:54 +0200

    personalizacja kategorii za pomocą danych pobieranych z firebase

--- NewsHUB/src/app/categoriesMenu/categoriesMenu.ts
+++ NewsHUB/src/app/categoriesMenu/categoriesMenu.ts
@@ -1,5 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {Router} from "@angular/router";
+import {CategoriesChoiceComponent} from "../categoriesChoice/categoriesChoice";
+import {AngularFireDatabase} from 'angularfire2/database-deprecated';
+import {FirebaseDatabase} from '@firebase/database-types';
+import {AngularFireAuth} from "angularfire2/auth";
 
 @Component({
   selector: 'catMenu',
@@ -7,17 +11,48 @@ import {Router} from "@angular/router";
   styleUrls: ['./categoriesMenu.css']
 })
 export class CatMenuComponent {
-  constructor(private router: Router){
 
+  constructor(private router: Router,db: AngularFireDatabase, auth: AngularFireAuth) {
+    this.db=db.database;
+    auth.authState.subscribe((user) => {
+      if(user) this.userID = user.uid;
+      this.getCatChoice()
+    });
   }
+
+  db:FirebaseDatabase;
+  userID:string;
+
+
   categories = [
-    {"id": 0, "name": "Business"},
-    {"id": 1, "name": "Entertainment"},
-    {"id": 2, "name": "Health"},
-    {"id": 3, "name": "Science"},
-    {"id": 4, "name": "Sports"},
-    {"id": 5, "name": "Technology"},
-    {"id": 6, "name": "General"}
+    {"id": 0, "name": "Business", "visible": true},
+    {"id": 1, "name": "Entertainment", "visible": true},
+    {"id": 2, "name": "Health", "visible": true},
+    {"id": 3, "name": "Science", "visible": true},
+    {"id": 4, "name": "Sports", "visible": true},
+    {"id": 5, "name": "Technology", "visible": true},
+    {"id": 6, "name": "General", "visible": true}
   ]
 
+
+  getCatChoice(){
+    let business = <HTMLInputElement> document.getElementById("businessInput");
+    let entertainment = <HTMLInputElement> document.getElementById("entertainmentInput");
+    let health = <HTMLInputElement> document.getElementById("healthInput");
+    let science = <HTMLInputElement> document.getElementById("scienceInput");
+    let sport = <HTMLInputElement> document.getElementById("sportInput");
+    let technology = <HTMLInputElement> document.getElementById("technologyInput");
+    this.db.ref('categories/'+this.userID).on('value', (snapshot) => {
+      this.categories[0].visible = snapshot.val().business;
+      this.categories[1].visible =  snapshot.val().entertainment ;
+      this.categories[2].visible =   snapshot.val().health;
+      this.categories[3].visible =  snapshot.val().science ;
+      this.categories[4].visible = snapshot.val().sport;
+      this.categories[5].visible = snapshot.val().technology;
+    });
+  }
+
 }
+
+
+
